f2607d9e7a92212b0ccf3a6358660cf1202c6d41
Tarique <tariqueanwer@outlook.com>
2017-03-03 01:34:01 +0530

    CMU112-Week2 Practice. nthPowerfulNumber

---
+++ src/Week2/Practice/nthPowerfulNumber.py
@@ -0,0 +1,83 @@
+import cs112_s17_linter
+import math
+
+def isPrime(n):
+    if n == 2:
+        return True
+    elif (n < 2) or (n % 2 == 0):
+        return False
+
+    for factor in range(3, math.floor(math.sqrt(n) + 1), 2):
+        if n % factor == 0:
+            return False
+    return True
+
+def isPowerfulNumber(n):
+    if n == 1: return True
+
+    isInvalid = False
+
+    for divisor in range(2, n + 1):
+        if n % divisor == 0 and isPrime(divisor):
+            if n % (divisor ** 2) != 0 :
+                isInvalid = True
+
+    return not isInvalid
+
+def nthPowerfulNumber(n):
+    found = 0
+    guess = 0
+
+    while found  <= n:
+        guess += 1
+        if isPowerfulNumber(guess):
+            found += 1
+    return guess
+
+def testNthPowerfulNumber():
+    print('Testing nthPowerfulNumber()... ', end='')
+    assert(nthPowerfulNumber(0) == 1)
+    assert(nthPowerfulNumber(1) == 4)
+    assert(nthPowerfulNumber(2) == 8)
+    assert(nthPowerfulNumber(3) == 9)
+    assert(nthPowerfulNumber(4) == 16)
+    assert(nthPowerfulNumber(5) == 25)
+    assert(nthPowerfulNumber(10) == 64)
+    assert(nthPowerfulNumber(15) == 121)
+    assert(nthPowerfulNumber(20) == 196)
+    assert(nthPowerfulNumber(36) == 500)
+    print('Passed.')
+
+
+#################################################
+# testAll and main
+#################################################
+
+def testAll():
+    testNthPowerfulNumber()
+
+def main():
+    bannedTokens = (
+        #'False,None,True,and,assert,def,elif,else,' +
+        #'from,if,import,not,or,return,' +
+        #'break,continue,for,in,while,' +
+        'as,class,del,except,finally,' +
+        'global,is,lambda,nonlocal,pass,raise,repr,' +
+        'try,with,yield,' +
+        #'abs,all,any,bool,chr,complex,divmod,float,' +
+        #'int,isinstance,max,min,pow,print,round,sum,' +
+        #'range,reversed,'+
+        '__import__,ascii,bin,bytearray,bytes,callable,' +
+        'classmethod,compile,delattr,dict,dir,enumerate,' +
+        'eval,exec,filter,format,frozenset,getattr,globals,' +
+        'hasattr,hash,help,hex,id,input,issubclass,iter,' +
+        'len,list,locals,map,memoryview,next,object,oct,' +
+        'open,ord,property,repr,set,' +
+        'setattr,slice,sorted,staticmethod,str,super,tuple,' +
+        'type,vars,zip,importlib,imp,string,[,],{,}')
+    cs112_s17_linter.lint(bannedTokens=bannedTokens) # check style rules
+    testAll()
+
+if __name__ == '__main__':
+    main()
+
