67db1b4758b3fcd342d6ad43980d0930ee5773fd
Tarique <tariqueanwer@outlook.com>
2017-03-13 20:36:10 +0530

    CMU112-Week3 Practice Exercises- areAnagrams

---
+++ src/Week3/Practice/areAnagrams.py
@@ -0,0 +1,53 @@
+import cs112_s17_linter
+import string
+
+def areAnagrams(s1, s2):
+    lookupString1 = ""
+    lookupString2 = ""
+
+    for c in string.ascii_lowercase:
+        lookupString1 += str(s1.lower().count(c))
+        lookupString2 += str(s2.lower().count(c))
+
+    return lookupString1 == lookupString2
+
+def testAreAnagrams():
+    print("Testing areAnagrams()...", end="")
+    assert(areAnagrams("", "") == True)
+    assert(areAnagrams("abCdabCd", "abcdabcd") == True)
+    assert(areAnagrams("abcdaBcD", "AAbbcddc") == True)
+    assert(areAnagrams("abcdaabcd", "aabbcddcb") == False)
+    print("Passed!")
+
+
+#################################################
+# testAll and main
+#################################################
+
+def testAll():
+    testAreAnagrams()
+
+def main():
+    bannedTokens = (
+        #'False,None,True,and,assert,def,elif,else,' +
+        #'from,if,import,not,or,return,' +
+        #'break,continue,for,in,while,' +
+        'as,class,del,except,finally,' +
+        'global,is,lambda,nonlocal,pass,raise,repr,' +
+        'try,with,yield,' +
+        #'abs,all,any,bool,chr,complex,divmod,float,' +
+        #'int,isinstance,max,min,pow,print,round,sum,' +
+        #'range,reversed,str,string,[,],ord,chr,input,len'+
+        '__import__,ascii,bin,bytearray,bytes,callable,' +
+        'classmethod,compile,delattr,dict,dir,enumerate,' +
+        'eval,exec,filter,format,frozenset,getattr,globals,' +
+        'hasattr,hash,help,hex,id,issubclass,iter,' +
+        'list,locals,map,memoryview,next,object,oct,' +
+        'open,property,repr,set,' +
+        'setattr,slice,sorted,staticmethod,super,tuple,' +
+        'type,vars,zip,importlib,imp,{,}')
+    cs112_s17_linter.lint(bannedTokens=bannedTokens) # check style rules
+    testAll()
+
+if __name__ == '__main__':
+    main()
