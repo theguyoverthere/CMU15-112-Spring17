f55159a753673f73b2f4308a0f6e32c36b640f3a
Tarique <tariqueanwer@outlook.com>
2017-03-14 21:34:59 +0530

    CMU112-Week3 Practice Exercises- replace

---
+++ src/Week3/Practice/replace.py
@@ -0,0 +1,75 @@
+import cs112_s17_linter
+
+def replace(s1, s2, s3):
+    previousResult = s1
+
+    if s2 == "" :
+        result = ""
+        for i in range(len(s1)):
+            result += s3 + s1[i]
+
+        return result + s3
+
+    while previousResult.find(s2) != -1:
+        if previousResult == s2: break
+
+        result = ""
+        result += (previousResult[:previousResult.find(s2)] +
+                   s3 +
+                   previousResult[previousResult.find(s2) + len(s2):])
+        previousResult = result
+
+    return previousResult
+
+def testReplace():
+    print("Testing replace()...", end="")
+    (s1, s2, s3) = ("abcde", "ab", "cd")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("abcdeabcde", "ab", "cd")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("babababa", "ab", "cd")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("abb", "ab", "a")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("", "ab", "a")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("abc", "", "q")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    (s1, s2, s3) = ("abc", "ab", "")
+    assert(replace(s1, s2, s3) == s1.replace(s2, s3))
+    print("Passed!")
+
+
+#################################################
+# testAll and main
+#################################################
+
+def testAll():
+    testReplace()
+
+def main():
+    bannedTokens = (
+        #'False,None,True,and,assert,def,elif,else,' +
+        #'from,if,import,not,or,return,' +
+        #'break,continue,for,in,while,' +
+        'as,class,del,except,finally,' +
+        'global,is,lambda,nonlocal,pass,raise,repr,' +
+        'try,with,yield,' +
+        #'abs,all,any,bool,chr,complex,divmod,float,' +
+        #'int,isinstance,max,min,pow,print,round,sum,' +
+        #'range,reversed,str,string,[,],ord,chr,input,len'+
+        '__import__,ascii,bin,bytearray,bytes,callable,' +
+        'classmethod,compile,delattr,dict,dir,enumerate,' +
+        'eval,exec,filter,format,frozenset,getattr,globals,' +
+        'hasattr,hash,help,hex,id,issubclass,iter,' +
+        'list,locals,map,memoryview,next,object,oct,' +
+        'open,property,repr,set,' +
+        'setattr,slice,sorted,staticmethod,super,tuple,' +
+        'type,vars,zip,importlib,imp,{,}')
+    cs112_s17_linter.lint(bannedTokens=bannedTokens) # check style rules
+    testAll()
+
+if __name__ == '__main__':
+    main()
+
+
