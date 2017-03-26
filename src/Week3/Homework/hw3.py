11509be6b7acc243f607d038aa87dfb9636eb1b2
Tarique <tariqueanwer@outlook.com>
2017-03-26 23:16:47 +0530

    CMU112-Week3 Homework complete. bonusEncode3( ) not attempted

--- src/Week3/Homework/hw3.py
+++ src/Week3/Homework/hw3.py
@@ -1,7 +1,6 @@
 #################################################
 # Hw3
 #################################################
-
 import cs112_s17_linter
 import math, string
 
@@ -25,20 +24,334 @@ def roundHalfUp(d):
 # Autograded Exercises
 #################################################
 
-def patternedMessage(msg, pattern):
-    return 42
+#################################################
+# patternedMessage(message, pattern)
+#################################################
+def patternedMessage(message, pattern):
+    result = ""
+    messageNoSpaces = ""
+
+    for i in range(len(message)):
+        if not message[i].isspace():
+            messageNoSpaces += message[i]
+
+    j = 0
+
+    for line in pattern.splitlines():
+        result += '\n'
+
+        for i in range(len(line)):
+            if line[i].isspace():
+                result += " "
+            else:
+                if j < len(messageNoSpaces):
+                    result += messageNoSpaces[j: j + 1]
+                else:
+                    j = 0
+                    result += messageNoSpaces[j: j + 1]
+                j += 1
+
+    return result
+
+#################################################
+# bestStudentAndAvg(gradebook)
+#################################################
 
 def bestStudentAndAvg(gradebook):
-    return 42
+    name = ""
+    bestStudent = ""
+    maxAverage = -99999999999999
+
+    for line in gradebook.splitlines():
+
+        if (len(line.strip()) == 0 or
+            line.startswith("#")   or
+            line.startswith(" ")):
+                continue
+        else:
+            sum = 0
+            count = 0
+
+            for word in line.split(","):
+                if word.isalpha():
+                    name = word
+                else:
+                    count += 1
+                    sum += int(word)
+
+            studentAverage = sum / count
+
+            if studentAverage >= maxAverage:
+                maxAverage = studentAverage
+                bestStudent = name
+
+    return bestStudent + ":" + str(roundHalfUp(maxAverage))
 
 ###### BONUS #######
 
-def topLevelFunctionNames(code): return 42
-def getEvalSteps(expr): return 42
-def bonusEncode1(msg): return 42
-def bonusDecode1(msg): return 42
-def bonusEncode2(msg): return 42
-def bonusDecode2(msg): return 42
+#################################################
+# topLevelFunctionNames(code)
+#################################################
+
+def hasEmbeddedQuote(line, quoteFound):
+
+    posDoubleQuote = line.find("\"\"\"")
+    posSingleQuote = line.find("\'\'\'")
+    posComment     = line.find("#")
+
+    if posComment != -1:
+        if line.find("\'#\'") == posComment - 1:
+            posComment = -1
+        elif line.find("\"#\"") == posComment - 1:
+            posComment = -1
+        elif line.find("\'\'\'#\'\'\'") == posComment - 1:
+            posComment = -1
+        elif quoteFound:
+            posComment = -1
+
+    if posDoubleQuote == -1 and posSingleQuote == -1:
+        return False
+    elif posComment == -1:
+        return True
+    elif (posDoubleQuote != -1) and (posDoubleQuote < posComment):
+        return True
+    elif (posSingleQuote != -1) and (posSingleQuote < posComment):
+        return True
+    else:
+        return False
+
+def extractFunctionName(line):
+
+    function = ""
+
+    if line.find(")") > line.find("<"):
+        start = line.find(" ") + 1
+        end   = line.find("(")
+
+        function = line[start : end]
+
+    return function
+
+def topLevelFunctionNames(code):
+    result = ""
+    quoteFound = False
+
+    for line in code.splitlines():
+        if quoteFound:
+            if hasEmbeddedQuote(line, quoteFound):
+                quoteFound = False
+            continue
+
+        if line.startswith("def "):
+            topLevelFunction = extractFunctionName(line)
+
+            if not topLevelFunction ==  "":
+                if result.find(topLevelFunction) == -1:
+                    if result == "":
+                        result += topLevelFunction
+                    else:
+                        result += "." + topLevelFunction
+
+            #Look for triple quotes
+            if hasEmbeddedQuote(line, quoteFound):
+                quoteFound = True
+
+    return result
+
+#################################################
+# getEvalSteps(expression)
+#################################################
+
+def binaryOperatorPresent(expression):
+    if expression.find("+") != -1:
+        return True
+    elif expression.find("-")  != -1:
+        return True
+    elif expression.find("*")  != -1:
+        return True
+    elif expression.find("/")  != -1:
+        return True
+    elif expression.find("%")  != -1:
+        return True
+    elif expression.find("//") != -1:
+        return True
+    elif expression.find("**") != -1:
+        return True
+
+    return False
+
+def nextOperator(expression):
+    if expression.find("**") != -1:
+        return "**"
+    elif expression.find("*")  != -1:
+        return "*"
+    elif expression.find("%")  != -1:
+        return "%"
+    elif expression.find("//") != -1:
+        return "//"
+    elif expression.find("/")  != -1:
+        return "/"
+    elif expression.find("+")  != -1:
+        return "+"
+    elif expression.find("-")  != -1:
+        return "-"
+    return 0
+
+def getLeftOperand(expression, index):
+
+    for i in range(index, 0, -1):
+
+        if (expression[i - 1: i] == "*"  or
+            expression[i - 1: i] == "/"  or
+            expression[i - 1: i] == "//" or
+            expression[i - 1: i] == "%"  or
+            expression[i - 1: i] == "+"  or
+            expression[i - 1: i] == "-"):
+
+            return int(expression[i: index + 1].strip()), i
+
+    return int(expression[0 : index + 1]), 0
+
+def getRightOperand(expression, index):
+
+    for i in range(index, len(expression)):
+
+        if (expression[i : i + 1] == "*"  or
+            expression[i : i + 1] == "/"  or
+            expression[i : i + 1] == "//" or
+            expression[i : i + 1] == "%"  or
+            expression[i : i + 1] == "+"  or
+            expression[i : i + 1] == "-"):
+
+            return (int(expression[index : i].strip()), index,
+                    len(expression[index : i].strip()))
+
+    return int(expression[index :]), index, len(expression[index :])
+
+def applyOperator(expression, operator):
+    index = expression.find(operator)
+
+    leftOperand, leftIndex = getLeftOperand(expression, index - 1)
+
+    if operator == "**" or operator == "//":
+        rightOperand, rightIndex, size = getRightOperand(expression, index + 2)
+    else:
+        rightOperand, rightIndex, size = getRightOperand(expression, index + 1)
+
+    if operator == "**":
+        operationResult = leftOperand ** rightOperand
+    elif operator == "*":
+        operationResult = leftOperand * rightOperand
+    elif operator == "/":
+        operationResult = leftOperand / rightOperand
+    elif operator == "//":
+        operationResult = leftOperand // rightOperand
+    elif operator == "%":
+        operationResult = leftOperand % rightOperand
+    elif operator == "+":
+        operationResult = leftOperand + rightOperand
+    elif operator == "-":
+        operationResult = leftOperand - rightOperand
+
+    return (expression[: leftIndex] +
+            str(operationResult)       +
+            expression[rightIndex + size :])
+
+def getPrefix(expression):
+    prefix = ""
+
+    for i in range(len(expression)):
+        prefix += " "
+
+    return prefix
+
+def getEvalSteps(expression):
+    prefix = getPrefix(expression)
+    reducedExpression = expression
+    result = ""
+
+    while binaryOperatorPresent(reducedExpression):
+        operator = nextOperator(reducedExpression)
+        reducedExpression = applyOperator(reducedExpression, operator)
+        if result == "":
+            result += expression + " = " + reducedExpression + "\n"
+        else:
+            result += prefix + " = " + reducedExpression + "\n"
+
+    if result == "":
+        return expression + " = " + expression
+    else:
+        return result[: len(result) - 1]
+
+#################################################
+# bonusEncode1(msg)
+#################################################
+
+def bonusEncode1(msg):
+	result = ""
+
+	for c in msg:
+		if (c.islower()):
+			c = chr(ord('a') + (ord(c) - ord('a') + 1) % 26)
+		result += c
+
+	return result
+
+#################################################
+# bonusDecode1(msg)
+#################################################
+
+def bonusDecode1(msg):
+	result = ""
+
+	for c in msg:
+		if c.islower():
+			c = chr(ord('a') + (ord(c) - ord('a') - 1) % 26)
+		result += c
+
+	return result
+
+
+#################################################
+# bonusEncode2(msg)
+#################################################
+
+import string
+import random
+
+#0    5    10   15   20    25   30   35    40   45   50   55   60   65   70   75
+#+----+----+----+----+-- --+----+----+--- -+----+----+----+----+----+----+----+
+#ddd 7jhnkvd1c00N(5aX):\n    0MZ0QX = ""\n    I = HHEuyq.izinm_nftscoo + kkh7b3
+
+# To encode: Fetch the position of each character in msg, and subtract that
+# value from the character itself. Wrap around if required.
+
+def bonusEncode2(msg):
+    result = ""
+    p = string.ascii_letters + string.digits
+    for i in range(len(msg)):
+        c = msg[i]
+        if (c in p): c = p[(p.find(c) - i) % len(p)]
+        result += c
+    return result
+
+#################################################
+# bonusDecode2(msg)
+#################################################
+
+def bonusDecode2(msg):
+    reference = string.ascii_letters + string.digits
+    referenceLength = len(reference)
+    result = ""
+
+    for i in range(len(msg)):
+        if msg[i].isalnum():
+            result += reference[(reference.find(msg[i]) + i) % referenceLength]
+        else:
+            result += msg[i]
+
+    return result
+
 def bonusEncode3(msg): return 42
 def bonusDecode3(msg): return 42
 
@@ -50,8 +363,62 @@ def bonusDecode3(msg): return 42
 # playPig (be sure this is below the # ignore_rest line!
 #################################################
 
+import random
+
+def playNextRound(playerScore):
+    WINNING_SCORE = 100
+
+    option = input("Roll dice (y/n)?").lower()
+    while not (option == 'y' or option == 'n'):
+                option = input("Please enter a valid option (y/n)?").lower()
+
+    currentScore = playerScore
+
+    while option == "y":
+        diceRoll = random.randint(1, 6)
+
+        if diceRoll == 1:
+            print("You rolled 1!")
+            break
+
+        currentScore += diceRoll
+
+        if currentScore >= WINNING_SCORE:
+            print("Current Score: ", currentScore, end="\n")
+            print("Congratulations, you've won!")
+            return -1
+        else:
+            print("Current Score: ", currentScore, end="\n")
+            option = input("Roll again (y/n)?").lower()
+
+            while not (option == 'y' or option == 'n'):
+                option = input("Please enter a valid option (y/n)?").lower()
+
+    return currentScore
+
 def playPig():
-    print("playPig() not yet implemented.")
+    scoreA = 0
+    scoreB = 0
+    currentPlayer = "Player A"
+
+    while True:
+        print("\n" + currentPlayer)
+
+        if currentPlayer == "Player A":
+            scoreA = playNextRound(scoreA)
+            currentScore = scoreA
+        else:
+            scoreB = playNextRound(scoreB)
+            currentScore = scoreB
+
+        if currentScore == -1 :
+            break
+        elif currentPlayer == "Player A":
+            currentPlayer = "Player B"
+        else:
+            currentPlayer = "Player A"
+
+    return 0
 
 #################################################
 # Test Functions
@@ -164,11 +531,11 @@ A-C D?A -CD
         soln = solns[i]
         soln = soln.strip("\n")
         observed = patternedMessage(msg, pattern)
-        #observed = patternedMessage(msg, pattern).strip("\n")
-        #print "\n\n***********************\n\n"
-        #print msg, pattern
-        #print "<"+patternedMessage(msg, pattern)+">"
-        #print "<"+soln+">"
+        observed = patternedMessage(msg, pattern).strip("\n")
+        # print("\n\n***********************\n\n")
+        # print(msg, pattern)
+        # print("<"+patternedMessage(msg, pattern)+">")
+        # print("<"+soln+">")
         assert(observed == soln)
     print("Passed!")
 
@@ -328,7 +695,7 @@ def testAll():
     testBonusGetEvalSteps()
     testBonusDecode1()
     testBonusDecode2()
-    testBonusDecode3()
+    # testBonusDecode3()
 
 def main():
     bannedTokens = (
