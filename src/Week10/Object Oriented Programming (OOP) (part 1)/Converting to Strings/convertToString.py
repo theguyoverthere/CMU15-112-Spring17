dae3716385568d9edb62c20ccb45f28145e75d5c
Tarique <tariqueanwer@outlook.com>
2017-12-25 21:27:32 +0530

    CMU112-Week10 Object Oriented Programming review.
    Reviews the concepts from Week10 Object Oriented Programming, since it's
    been a while.

--- src/Week10/Object Oriented Programming (OOP) (part 1)/Converting to Strings/convertToString.py
+++ src/Week10/Object Oriented Programming (OOP) (part 1)/Converting to Strings/convertToString.py
@@ -2,8 +2,8 @@ class A(object):
     def __init__(self, x):
         self.x = x
 
-    # def __str__(self):
-    #     return "A(%d)" %self.x
+    def __str__(self):
+        return "A(%d)" %self.x
 
     def __repr__(self):
         return "A(%d)" % self.x
